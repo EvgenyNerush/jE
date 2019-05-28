ed03a80be4258cbbd0b0248f7e0d2c8a28206780
ai94 <49003452+ai94@users.noreply.github.com>
2019-05-28 13:23:03 +0300

    Add files via upload

--- gr.py
+++ gr.py
@@ -4,20 +4,35 @@ import numpy.ctypeslib as ctl
 import ctypes as ct
 import matplotlib.pyplot as plt
 import statistics
+import seaborn as sns
 import random
-def radp(x): return np.sin(x)**2
-n = 100000
+import scipy.special as sp
+gamma = 100
+beta = np.sqrt(1.0 - 1.0/gamma**2.0)
+h = 0.0001
+omega0 = h/gamma
+def bk(x):
+    return x*x * sp.kv(2/3,(x/omega0)*(beta/gamma**3.0)*(1.0/3.0) )**2*0.08# omega^2!!!
+n = 10000
 subprocess.run(["make"])
 main = ctl.load_library("main.so","./")
 main.metropolis_spectrum_sy.restype = ct.POINTER(ct.c_double)
 x_p = main.metropolis_spectrum_sy(n)
 x = ctl.as_array(x_p, shape = (n,))
-omega = []
-for i in range(int(n/2)):
-    if i % 2 ==0:
-        omega.append(x[i])
-#weights=x,
-plt.hist(x,bins=90,normed=True,weights=x,color='r')
-plt.xlabel('omega, mc^2/h')
-plt.ylabel('f(omega), a.u.')
+M = 10000
+y = 10*np.array(range(0,M+1))/M
+
+#plt.hist(x,bins=80,normed=True,weights=x,color='r',alpha=0.5,edgecolor='k')
+plt.hist(x,bins=80,density=True,weights=x,color='r')
+plt.plot(y,bk(y),color='g')
+
+plt.xlabel(r'$\omega$, plank units.')
+plt.ylabel(r'f($\omega$,$\theta = 0$), a.u.')
+#plt.xlim(0,10)
+#plt.text(-0.023,30,r'$\sigma = %.5f$'%(np.std(x)),fontsize=15,color='k')
+#plt.text(0,0.6,r'$\sin(\theta)^{2}$',fontsize=15,color='blue')
+#plt.xlabel('omega, mc^2/h')
+#plt.ylabel('f(omega), a.u.')
+#print(np.std(x))
+plt.tick_params(axis='both',direction='in')
 plt.show()
--- main.cpp
+++ main.cpp
@@ -4,160 +4,91 @@
 #include<random>
 #include<complex>
 
-const long double pi = M_PI;
+const double pi = M_PI;
 // all quantities in Plank units (\hbar = 1, c = 1, G = 1)
-double h = 0.0005;// h = H/H_cr - magnetic field Schwinger field(H_cr = m^2 c^3 /(e\hbar) ) ratio
+double h = 0.0001;// h = H/H_cr - magnetic field Schwinger field(H_cr = m^2 c^3 /(e\hbar) ) ratio
 const double m = 1.0; // lepton/proton mass in electron masses
-const double gamma0 = 100.0;// = energy / (m_{particle} c^2)
-double omega_cr = 0.45*h * gamma0*gamma0;//
+double gamma0 = 100.0;// = energy / (m_{particle} c^2)
+double beta = sqrt( 1 - 1/(gamma0 * gamma0) );
+double omega0 = h/(m*m*gamma0);
+double tcoh = 1.0*m*m / h;// coherence time /formation time
+double omega_cr = 0.3*h * gamma0*gamma0;
 using namespace std;
 typedef complex<double> dcomp;
 dcomp i(0,1);
 
-double ind(double h,double omega){
-  double kappa = h*omega;
+double ind(double h1,double omega){
+  double kappa = h1*omega;
+  double dn = 0;
   if (kappa <= 2.01523 ){
-    return 0.22;
+    dn = 0.22;
   } else{
-    return 0.56 / pow(kappa,4.0/3.0);
+    dn = 0.56 / pow(kappa,4.0/3.0);
   }
+  return 1 + 1.0 * h1 * h1 * dn /(pi * 137.0);
 }
 
-dcomp dipole_jE(double omega, double theta,double t){
-
-  double omega0 = 1;// \hbar / (m_e * c^2) units
-  double a = 0.01;// a = dipole_length / lambda0
-  //k = omega / c
-  double kd = a * 2 * pi * omega / omega0 ;//
-  return sin(theta) * cos(omega0 * t) * exp( -i*omega * t + i * kd * cos(theta) * sin(omega0 * t) );
+double tau1(double omega){
+  return (gamma0 - omega/m) / (gamma0 * omega * abs(1 - beta ) );
+}
+double tau2(double omega){
+  return pow( 6 * (gamma0 - omega/m) / (omega *gamma0 * omega0*omega0) , 1.0/3.0 );
 }
 
-double c1d(double t1,double t2,double omega, double theta){
-  const int m = 1000;
-  dcomp sum(0,0);
-  for (unsigned int j = 0;j <= m;j++){
-    sum += dipole_jE(omega, theta, t1 + (t2 - t1) * j / m);
-  }
-  return pow(abs(sum),2.0);
+dcomp sync_jE1(double omega,double theta,double t){
+  double ksi = sqrt(2)*(1 - beta)*sqrt(1 - beta)/omega0;
+  double phi = ksi * omega * (t + t*t*t/3);
+  double zu = omega0/sqrt(2*(1 - beta));
+  //double phi =  t/tau1(omega) + pow(t/ tau2(omega),3.0);
+  //double phi = ( omega*t*(1 - beta) + omega*omega0*omega0*t*t*t/6 ) /(1.0 - omega /(m* gamma0) );
+  //double phi =  omega*( t - beta * sin(omega0*t) * cos(theta) / omega0 );
+  //return exp( -i*phi )*t;
+  return exp(-i*phi)*sin(t*omega0/zu);
 }
 
-dcomp sync_jE(long double omega,double theta,long double t){
-  // all quantities in Plank units
-  // rotating particle
-  long double beta = sqrt( 1 - 1/(gamma0 * gamma0) );
-  long double omega0 =  h /(gamma0 * m * m);
-  //double n = 1 + 1.0 * h * h * ind(h,omega) / ( pi * 137.0);//index of refraction (Re part)
-  double tau1 = (gamma0 - omega/m) / (gamma0 * omega * (1 - beta) );
-  double tau2 = pow( 6 * (gamma0 - omega/m)*m*m*m*m*gamma0 / (omega * h * h) , 1.0/3.0 );
-
-  double phi = t/tau1 + pow(t / tau2,3.0);
-  //double phi =  omega*( t - beta * sin(omega0*t) * cos(theta) / omega0 ) /(1.0 - omega / (m*gamma0));
-  //long double ksi = gamma0 - omega;
-  //double phi = t *( 1 + h*h*t*t/3 )/(2*ksi );
-  return exp( -i*phi );
-  //return cos(theta) * exp( -i * omega * t * ( (1 - beta * cos(theta)) + omega0 * omega0 * t * t /6 ) * gamma0/(gamma0 - omega / m) );
-}
-
-long double c1s(long double t1,long double t2,double omega, double theta){
+double c1s(double t1,double t2,double omega, double theta){
   // calculating |C_1|^2
-  const int n = 1000;
+  double step = 0.001;
+
+  size_t n = (t2-t1)/step;
   dcomp sum(0,0);
-  for (unsigned int j = 0;j <= n;j++){
-    sum += sync_jE(omega, theta, t1 + (t2 - t1) * j / n);
+  for (unsigned int j = 0;j < n;j++){
+    sum += sync_jE1(omega, theta, t1 + step*j)*step;
   }
+
+  if (abs(sum) > ( 200 )*(t2 - t1)*step*step/24 ){
+    return pow(abs(sum),2.0);
+  }
+  else {
+    return 0.0;
+  }
+
   return pow(abs(sum),2.0);
 }
 
 extern "C" {
-
-  double* metropolis_integration(size_t n){
-
-    default_random_engine gen{static_cast<long unsigned int>(time(NULL))};
-    uniform_real_distribution<double> omega_dist(0.01,gamma0);
-    uniform_real_distribution<double> angle_dist(-20.0/gamma0,20.0/gamma0);
-    uniform_real_distribution<double> r(0,1);
-    double* x = new double[size_t (2*n)];
-    x[0] = 100;// even - ang. freq. (omega)
-    x[1] = 0.0;// odd - angle
-    double form_time = 1.0*m*m / h;// coherence time /formation time
-    double t1 = - form_time / 2.0;
-    double t2 = form_time / 2.0;
-    double tmp = c1s(t1,t2,x[0],x[1]);
-    for(unsigned int j = 1; j < n; ++j){
-      double omega = omega_dist(gen);
-      double angle = angle_dist(gen);
-      double s = c1s(t1,t2,omega,angle);
-      if (s >= tmp){
-        x[2*j] = omega;
-        x[2*j+1] = angle;
-        tmp = s;
-      } else if (s > tmp * r(gen)){
-        x[2*j] = omega;
-        x[2*j+1] = angle;
-        tmp = s;
-      } else {
-        x[2*j] = x[2*(j-1)];
-        x[2*j+1] = x[2*j-1];
-      }
-    }
-    return x;
-  }
-
   double* metropolis_spectrum_sy(size_t n){
     // spectrum for a given angle!
     default_random_engine gen{static_cast<long unsigned int>(time(NULL))};
-    uniform_real_distribution<long double> omega_dist(0.000001,gamma0);//
-    uniform_real_distribution<long double> r(0,1);
-    double* x = new double[n];
-    x[0] = 1.0;//seed value
-    double angle = 0.0;
-    double tc = 1.0*m*m / h;// coherence time /formation time
-
-    long double tau2 = pow( 6 * (gamma0 - x[0]/m)*m*m*m*m*gamma0 / (x[0] * h * h) , 1.0/3.0 );// tau2
-    long double tau1 = (gamma0 - x[0]/m)/(gamma0 * x[0] * (1 - sqrt(1 - 1/(gamma0 * gamma0))) );
-    long double form_time = tau2;
-
-    long double tmp = c1s( -form_time / 2.0,form_time / 2.0,x[0],angle);// |C_m|^2
-    for(unsigned int j = 1; j < n; ++j){
-      double omega = omega_dist(gen);
-
-      tau2 = pow( 6 * (gamma0 - omega/m)*m*m*m*m*gamma0 / (omega * h * h) , 1.0/3.0 );
-      tau1 = (gamma0 - omega/m) / (gamma0 * omega * (1 - sqrt(1 - 1/(gamma0 * gamma0))) );
-      form_time = 10*tc;
-
-      long double s = c1s( -form_time / 2.0,form_time / 2.0,omega,angle);
-      if (s >= tmp){
-        x[j] = omega;
-        tmp = s;
-      } else if (s > tmp * r(gen)){
-        x[j] = omega;
-        tmp = s;
-      } else {
-        x[j] = x[j-1];
-      }
-    }
-    return x;
-  }
-
-  double* metropolis_radpattern_sy(size_t n){
-    // radiation pattern for a given frequency
-    default_random_engine gen{static_cast<long unsigned int>(time(NULL))};
-    uniform_real_distribution<double> distribution(-20.0/gamma0,20.0/gamma0);
+    uniform_real_distribution<double> omega_dist(0.0001,10);//
     uniform_real_distribution<double> r(0,1);
     double* x = new double[n];
-    x[0] = 1.0;//seed value
-    double form_time = 1.0*m*m / h;// coherence time /formation time
-    double t1 = - form_time / 2.0;
-    double t2 = form_time / 2.0;
-    double tmp = c1s(t1,t2,omega_cr, x[0]);
-    for(unsigned int j = 1; j<n; ++j){
-      double tmp_x = distribution(gen);
-      double s = c1s(t1,t2,omega_cr,tmp_x);
+    x[0] = 0.1488;//seed value
+    double angle = 0.0;
+    double form_time = 40;
+
+    double tmp = c1s( -form_time / 2.0,form_time / 2.0,x[0],angle);// |C_m|^2
+    for(unsigned int j = 1; j < n; ++j){
+      double omega = omega_dist(gen);
+      form_time = 40;
+
+      //form_time = tau2(omega) * pow( tau2(omega)/tau1(omega) , 0.5);
+      double s = c1s( -form_time / 2.0,form_time / 2.0,omega, angle) * omega;
       if (s >= tmp){
-        x[j] = tmp_x;
+        x[j] = omega;
         tmp = s;
       } else if (s > tmp * r(gen)){
-        x[j] = tmp_x;
+        x[j] = omega;
         tmp = s;
       } else {
         x[j] = x[j-1];
