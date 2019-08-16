570042d16095ca47038fcb4897012ba96abf7387
Evgeny Nerush <nerush@appl.sci-nnov.ru>
2019-08-17 00:18:36 +0300

    -ffast-math added in tests/metropolis.cpp

--- tests/metropolis.cpp
+++ tests/metropolis.cpp
@@ -1,5 +1,6 @@
 /* In this test *n* x-coordinates with *target_f* distribution are generated, then the average
  * value of x, x^2 and x^3 are computed and compared with theoretical values.
+ * This test takes about two seconds with Xeon X5550 processor.
  */
 
 #include <iostream>
@@ -7,6 +8,19 @@
 #include <stdexcept>
 #include <cmath>
 
+// Park--Miller RNG
+const uint64_t pm_randmax = 0x7FFFFFFFull; // approx 2*10^9
+uint64_t pm_rng(uint64_t r) {
+    return 48271ull * r % pm_randmax;
+}
+
+// Float (or double) in (0, 1), computed from value returned by pm_rng
+template<typename F>
+F to_floating_01(uint64_t r) {
+    return static_cast<F>(r) / static_cast<F>(pm_randmax);
+}
+
+// target function used in this test
 double target_f(double x){
     if (x > 0 and x <= 1) {
         return 1 / sqrt(x);
@@ -15,16 +29,10 @@ double target_f(double x){
     }
 }
 
-// Park--Miller RNG
-const uint64_t pm_randmax = 0x7FFFFFFFull;
-uint64_t pm_rng(uint64_t r) {
-    return 48271ull * r % pm_randmax;
-}
-// to_Type( p / pm_randmax)...
 // walk function of (rng_state, x)
 std::pair<uint64_t, double> walk_f(std::pair<uint64_t, double> rx) {
     uint64_t r = pm_rng(rx.first);
-    double d = static_cast<double>(r) / static_cast<double>(pm_randmax);
+    double d = to_floating_01<double>(r);
     //std::cout << rx.second << '\t' << 0.5 * (2 * d - 1) << '\n';
     return std::make_pair(r, rx.second + 0.5 * (2 * d - 1));
 }
@@ -46,10 +54,6 @@ double theor_mean_x_pow(int pw){
     }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//uint32_t pm_state = 42;
-//
 // random numbers generated with metropolis algorithm
 std::vector<double> metropolis( double target_f(double)
                               , size_t n
@@ -64,10 +68,9 @@ std::vector<double> metropolis( double target_f(double)
     for (size_t i = 1; i < n; ++i) {
         rx = walk_f(rx);
         double acceptance = target_f(rx.second) / target_f(x[i - 1]);
-        // избежать срыва конвейера?
         if (acceptance < 1) {
             s = pm_rng(s);
-            if (static_cast<double>(s) / static_cast<double>(pm_randmax) >= acceptance) {
+            if (to_floating_01<double>(s) >= acceptance) {
                 rx.second = x[i - 1];
             }
         }
@@ -75,23 +78,17 @@ std::vector<double> metropolis( double target_f(double)
     }
     return x;
 }
-///////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main() {
-    size_t n = 1000000;
-    double acceptable_accuracy = 5 / sqrt(static_cast<double>(n));
+    size_t n = 16000000; // should be much less than pm_randmax
+    double acceptable_accuracy = 10 / sqrt(static_cast<double>(n));
     std::vector<double> xs = metropolis(target_f, n, walk_f, std::make_pair(12345, 0.5), 123);
-    /*for (auto x: xs) {
-        std::cout << x << '\n';
-    }
-    */
     std::vector<int> pws {1, 2, 3};
     std::vector<double> relative_errs;
     bool acc = true;
     for (auto pw: pws) {
         double relative_err
             = fabs(mean_x_pow(xs, pw) - theor_mean_x_pow(pw)) / theor_mean_x_pow(pw);
-        // std::cout << mean_x_pow(xs, pw) << '\t' << theor_mean_x_pow(pw) << '\n';
         relative_errs.push_back(relative_err);
         acc = acc and relative_err < acceptable_accuracy;
     }
