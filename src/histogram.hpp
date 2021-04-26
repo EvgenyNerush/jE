63a48c4227063b0a3f3f3af722408f1fb964f271
Evgeny Nerush <nerush@appl.sci-nnov.ru>
2021-04-26 10:41:10 +0300

    bugfix for a single dot in a single bin

--- src/histogram.hpp
+++ src/histogram.hpp
@@ -31,7 +31,7 @@
 std::vector<double> histogram_1D(size_t n, const std::vector<double>& xs){
     std::vector<double> bs; // boundaries of the bins
 
-    if (xs.size() > 1) { // for a single point the bin width cann't be found
+    if (n >= 1 && xs.size() >= n) { // the bin width cann't be found otherwise
         std::vector<double> ys(xs.size()  - xs.size() %  n);
         std::copy(xs.cbegin(), xs.cend() - xs.size() %  n, ys.begin());
 
